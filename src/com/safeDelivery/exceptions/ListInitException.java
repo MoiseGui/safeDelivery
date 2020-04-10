9ad06c0e72b40298368f92a30c0c3f8327003416
yassir acaf <yassir.acaf@gmail.com>
2020-04-10 21:06:26 +0100

    Add jars

---
+++ src/com/safeDelivery/exceptions/ListInitException.java
@@ -0,0 +1,12 @@
+package com.safeDelivery.exceptions;
+
+import java.util.List;
+
+public class ListInitException  extends Exception{
+
+    public ListInitException ()
+    {
+     System.out.println("les listes sont vides");
+    }
+    
+}
